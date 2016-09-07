96e94ae41643372e2942c492cd5a357ca3925678
Frank Cardillo <facardillo91@gmail.com>
2016-09-07 16:00:40 -0400

    finished codecademy lesson 1, unit 1

---
+++ codecademy-lessons/LearnReactJSPart1/unit-1/13-passing-a-variable-to-ReactDOM-render.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Write code here:
+var myList = (
+  <ul>
+  	<li>First</li>
+    <li>Second</li>
+    <li>Third</li>
+    <li>Fourth</li>
+  </ul>
+);
+
+ReactDOM.render(myList, document.getElementById('app'));
