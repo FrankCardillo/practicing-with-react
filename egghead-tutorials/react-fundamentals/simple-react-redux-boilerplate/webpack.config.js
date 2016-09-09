db8e67b4bc8f3ac88c340d77b539d8781f026104
Frank Cardillo <facardillo91@gmail.com>
2016-09-09 16:32:12 -0400

    getting set up for egghead react tutorials

---
+++ egghead-tutorials/react-fundamentals/simple-react-redux-boilerplate/webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    path: './src/main.js'
+  },
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './build',
+    inline: true,
+    hot: true,
+    port: 3333
+  }
+}
