c6695dc26a44a438ecad9f7c17d8116ce88eaea3
Frank Cardillo <facardillo91@gmail.com>
2016-09-09 16:39:01 -0400

    point of reference to older version of component

--- egghead-tutorials/react-fundamentals/simple-react-redux-boilerplate/src/App.js
+++ egghead-tutorials/react-fundamentals/simple-react-redux-boilerplate/src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h1>
+          Hello World
+        </h1>
+        <b>Bold</b>
+      </div>
+    ); //React.createElement('h1', null, 'Hello World')
   }
 }
+
+// const App = () => <h1>Hello Eggheads</h1>
 
 export default App;
